7e3fea842ddefe8d0471ae4f5e4535b450237e86
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2023-11-30 04:14:36 +0530

    learning diff type of variable assignments

---
+++ variables/src/main.rs
@@ -0,0 +1,21 @@
+
+
+fn main() {
+
+    let x: i32 = -3;
+    let mut y: i128 = 2423783192401;
+    let z: f64 = 3.14;
+    let p: bool = !true;
+
+let q: char = 'o';let r: &str = "him n";
+
+println!("{} {}",x+x, y);   let x: i64 = 412293813135;y = 2344;     println!("
+Int 32 : {x}
+Int 64 : {y}
+Float 64 : {z}
+Boolean : {p}
+Character : {q} 
+String : {r}"
+);
+
+}

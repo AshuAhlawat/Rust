0269a5b387d35044fd62abb4d50bc99065ab8a0f
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2024-01-29 16:28:32 +0530

    tupples

--- tuppy/src/main.rs
+++ tuppy/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
+    let mut x = 0;
+    {
+        println!("{x}");
+        let tup1 = (2,45,-3.14, false, (5, 3));
+        println!("{} {}", tup1.4.1, tup1.2);
+        x = 1;
+    }
+    
+    // println!("{}", tup1.1)
 
-    let tup1 = (2,45,-3.14, false, (5, 3));
-    println!("{}", tup1.-1.-1)
+    println!("{}", x);
+
+    {
+        let x = true;
+    }
+
+    println!("{}", x)
+
 
 }
